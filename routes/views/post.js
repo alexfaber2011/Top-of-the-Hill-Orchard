34c9cb76ba176f32188371aa33546099b6b4f4ae
Alex Faber <alexfaber2011@gmail.com>
2017-09-05 07:27:57 -0700

    actually commit the two files for the previous commit… shwoops.

--- routes/views/post.js
+++ routes/views/post.js
@@ -14,32 +14,7 @@ exports = module.exports = function (req, res) {
 		posts: [],
 	};
 
-	// Load the current post
-	view.on('init', function (next) {
-
-		var q = keystone.list('Post').model.findOne({
-			state: 'published',
-			slug: locals.filters.post,
-		}).populate('author categories');
-
-		q.exec(function (err, result) {
-			locals.data.post = result;
-			next(err);
-		});
-
-	});
-
-	// Load other posts
-	view.on('init', function (next) {
-
-		var q = keystone.list('Post').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('4');
-
-		q.exec(function (err, results) {
-			locals.data.posts = results;
-			next(err);
-		});
-
-	});
+	
 
 	// Render the view
 	view.render('post');
