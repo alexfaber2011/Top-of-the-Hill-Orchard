764a1dbdd52d84ce50fae2995a4b67f32495c18d
Alex Faber <alexfaber2011@gmail.com>
2017-09-21 07:14:23 -0700

    allow the creation of landing page photos, for now, until I draft up a way to bootstrap them

--- models/LandingPagePhoto.js
+++ models/LandingPagePhoto.js
@@ -9,8 +9,8 @@ var Types = keystone.Field.Types;
 var LandingPagePhoto = new keystone.List('LandingPagePhoto', {
 	map: { name: 'title' },
 	autokey: { path: 'slug', from: 'title', unique: true },
-	nocreate: true,
-	nodelete: true,
+	nocreate: false,
+	nodelete: false,
 });
 
 LandingPagePhoto.add({
